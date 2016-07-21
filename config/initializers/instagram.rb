7f17474843f0b187d9eacb07abb697d298d0c4ab
Juan Pablo Ugas <PabloUgas@Juans-MacBook-Pro.local>
2016-07-21 13:21:36 -0700

    fixing instagram access_token

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -4,6 +4,6 @@
 
     config.client_id = "97bca408783443179b4db920c76712ff"
 
-    config.access_token = "d918fff0f1cb4584a8aa97deae4de092"
+    config.access_token = "3560974685.1677ed0.41d633cf58fa41c58f57099a319d935b"
 
     end
