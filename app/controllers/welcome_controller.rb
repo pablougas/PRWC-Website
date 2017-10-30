cfbbc3e81ef461d70ea84dc39a0756c0b67aab0f
Pablo Ugas <pablo.ugas@gmail.com>
2017-10-30 15:51:09 -0700

    member list and meetings now on dash

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
 
   def index
   	@projects = Project.joins(:user).where("user_id = users.id")
+    @meetings = Minute.where("date >= ?", Date.today).order("date ASC").limit(3)
   	@events = Event.where("date >= ?", Date.today).order("date ASC").limit(3)
   	@posts = Blog.where("created_at <= ?", Date.today).order("created_at DESC").limit(3)
 
