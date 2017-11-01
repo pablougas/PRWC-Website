caac27051132c3befcb026ca292745c52d98daf2
Pablo Ugas <pablo.ugas@gmail.com>
2017-11-01 10:30:56 -0700

    wip on gallery js fix

--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -1,7 +1,7 @@
 class Photograph < ActiveRecord::Base
     belongs_to :user
     belongs_to :album
-    has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", small:"200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :avatar, styles: { large: "600x600#", medium: "300x300>", small:"200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
     validates :avatar, attachment_presence: true
     validates_with AttachmentPresenceValidator, attributes: :avatar
